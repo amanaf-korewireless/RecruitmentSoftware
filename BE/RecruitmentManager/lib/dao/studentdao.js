422f7bbb044cf1723a47cb3f959195c622a2e301
Abdul Manaf <amanaf@korewireless.com>
2018-10-12 09:36:47 -0400

    modified registration api

---
+++ BE/RecruitmentManager/lib/dao/studentdao.js
@@ -0,0 +1,50 @@
+
+
+
+
+exports.insert = function (studentData, callback) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            callback(true, err);
+            return;
+        }
+        console.log("mongo connection ok");
+        var dbo = db.db("RecruitmentManager");
+        dbo.collection("student").insertOne(studentData, function (err, res) {
+            if (err) {
+                callback(true, err);
+                return;
+            }
+            console.log("1 document inserted");
+            db.close();
+            callback(null, true);
+            return;
+        });
+    });
+}
+
+exports.isAllreadyExist = function (studentData, callback) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            callback(true, err);
+            return;
+        }
+        console.log("mongo connection ok");
+        var dbo = db.db("RecruitmentManager");
+        var query = { email: studentData.email };
+        dbo.collection("student").find(query).toArray(function (err, result) {
+            if (err) {
+                callback(true, err);
+                return;
+            }
+            console.log(result);
+            db.close();
+            callback(null, false);
+            return;
+        });
+    });
+}
--- BE/RecruitmentManager/routes/student.js
+++ BE/RecruitmentManager/routes/student.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var validator =require('../lib/validator/schemaValidator.js');
-
+var studentDao=require('../lib/dao/studentdao.js');
 
 /* Register new student  */
 router.post('/', function(req, res, next) {
@@ -9,13 +9,33 @@ router.post('/', function(req, res, next) {
     //validate json schema
     validator.validateRegisterStudentSchema(req.body, function (err, reply) {
         if (err) {
-          res.status(400).send({ "errorMessage": "Incorrect Request Data Formatexpress-jsonschema: Invalid data found" ,"errorCode": "GEN001" })
+          res.status(400).send({ "errorMessage": "Incorrect Request Data Formatexpress-jsonschema: Invalid data found" ,"errorCode": "ER001" })
           return;
         }
         else {
           console.log("request body validation completed");
-          res.status(200).send({ "succuss":true,"msg": "student registration completed succussfully" })
-          return;
+          studentDao.isAllreadyExist(req.body,function(err,reply){
+            if(err)
+            {
+              res.status(400).send({ "errorMessage": "Server error" ,"errorCode": "ER002" })
+              return;
+            }
+            if(reply==true)
+            {
+              res.status(400).send({ "errorMessage": "Email already exist" ,"errorCode": "ER003" })
+              return;
+            }
+            studentDao.insert(req.body,function(err,reply){
+              if(err)
+              {
+                res.status(400).send({ "errorMessage": "Server error" ,"errorCode": "ER002" })
+                return;
+              }
+              console.log("student registration completed succussfully");
+              res.status(200).send({ "succuss":true,"msg": "student registration completed succussfully" })
+              return;
+            });
+          });
         }
     });
 });
