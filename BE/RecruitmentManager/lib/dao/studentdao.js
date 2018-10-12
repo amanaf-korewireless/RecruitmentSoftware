add9621c926b98f8e1c6ca3d4b5ab35d9b46b01d
Abdul Manaf <amanaf@korewireless.com>
2018-10-12 10:20:01 -0400

    added password encryption

--- BE/RecruitmentManager/lib/dao/studentdao.js
+++ BE/RecruitmentManager/lib/dao/studentdao.js
@@ -1,6 +1,27 @@
 
+var config = require('../config/serverConfig.json');
 
 
+
+function hashPassword(studentData, callback) {
+    bcrypt.genSalt(config.bcrypt.saltWorkFactor, function (err, salt) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        // hash the password along with our new salt
+        bcrypt.hash(studentData.password, salt, function (err, hash) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            // override the cleartext password with the hashed one
+            studentData.password = hash;
+            callback(null, 'ok');
+            return;
+        });
+    });
+}
 
 exports.insert = function (studentData, callback) {
     var MongoClient = require('mongodb').MongoClient;
@@ -11,16 +32,22 @@ exports.insert = function (studentData, callback) {
             return;
         }
         console.log("mongo connection ok");
-        var dbo = db.db("RecruitmentManager");
-        dbo.collection("student").insertOne(studentData, function (err, res) {
+        hashPassword(studentData, function (err, data) {
             if (err) {
                 callback(true, err);
                 return;
             }
-            console.log("1 document inserted");
-            db.close();
-            callback(null, true);
-            return;
+            var dbo = db.db("RecruitmentManager");
+            dbo.collection("student").insertOne(studentData, function (err, res) {
+                if (err) {
+                    callback(true, err);
+                    return;
+                }
+                console.log("1 document inserted");
+                db.close();
+                callback(null, true);
+                return;
+            });
         });
     });
 }
